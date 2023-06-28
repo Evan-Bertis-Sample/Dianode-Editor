28b89de771ac1bbc833d56dcfa620f017ee0f6c4
Shrimp-Chip <esample21@gmail.com>
2023-06-27 22:19:56 -0400

    Added imnodes library and custom CMake

--- make.sh
+++ make.sh
@@ -1,4 +1,4 @@
-cmake -GNinja -DCMAKE_BUILD_TYPE=Debug -DWARNINGS_AS_ERRORS=FALSE -B build/debug
+cmake -Wno-dev -GNinja -DCMAKE_BUILD_TYPE=Debug -DWARNINGS_AS_ERRORS=FALSE -B build/debug
 ninja -C build/debug
 cd ./build/debug/src/app && ./App
 cd ../../../..
